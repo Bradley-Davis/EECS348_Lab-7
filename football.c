cf0f26c37a5b4e602a55e565cfda484dcc3f77ee
brad4phillies <b758d196@ku.edu>
2025-03-12 15:56:22 -0500

    final commit

--- football.c
+++ football.c
@@ -1,40 +1,40 @@
 #include "football.h"
 #include <stdio.h>
-
+//initialize variables
 static int results[10000][5]; 
 static int count = 0;      
 
-int count_combinations(int points) {
+int count_combinations(int points) {//count the variations
     count = 0; 
 
-    for (int tds2 = 0; tds2 * 8 <= points; tds2++) {  
-        for (int tds1 = 0; tds2 * 8 + tds1 * 7 <= points; tds1++) {  
-            for (int tds = 0; tds2 * 8 + tds1 * 7 + tds * 6 <= points; tds++) {  
-                for (int fg = 0; tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 <= points; fg++) {
-                    for (int sf = 0; tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 + sf * 2 <= points; sf++) { 
-                        if (tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 + sf * 2 == points) {
-                            results[count][0] = tds2;
+    for (int tds2 = 0; tds2 * 8 <= points; tds2++) {  //determine the number of touch downs and 2 extra points
+        for (int tds1 = 0; tds2 * 8 + tds1 * 7 <= points; tds1++) {  //determine the number of touch downs and 1 extra point
+            for (int tds = 0; tds2 * 8 + tds1 * 7 + tds * 6 <= points; tds++) {  //determine the number of touch downs 
+                for (int fg = 0; tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 <= points; fg++) {//determine the number of Field goals
+                    for (int sf = 0; tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 + sf * 2 <= points; sf++) { //determine the number of safties
+                        if (tds2 * 8 + tds1 * 7 + tds * 6 + fg * 3 + sf * 2 == points) {//check if the combination creates the desired output number
+                            results[count][0] = tds2;//adds each amount to each category
                             results[count][1] = tds1;
                             results[count][2] = tds;
                             results[count][3] = fg;
                             results[count][4] = sf;
-                            count++;
+                            count++;//next iteration
                         }
                     }
                 }
             }
         }
     }
-    return count;
+    return count;//end function
 }
 
-void print_combinations(int points) {
+void print_combinations(int points) {//print the valid combinations
     int i;
-    int num_combinations = count_combinations(points);
+    int num_combinations = count_combinations(points);//initialize variabels
 
-    for (i = 0; i < num_combinations; i++) {
+    for (i = 0; i < num_combinations; i++) {//for every combination
         
-        printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n",
+        printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n",//print the amount of each variable
                results[i][0], results[i][1], results[i][2], results[i][3], results[i][4]);
     }
 }
--- football_main.c
+++ football_main.c
@@ -1,16 +1,16 @@
 #include "football.h"
 #include <stdio.h>
 
-void print_combinations(int points);
+void print_combinations(int points);//initialize function
 
-int main() {
-    int points = 2;
-    while (points>1){
+int main() {//start program
+    int points = 2;//initialize minimum points
+    while (points>1){//while the points is above 1 loop
         printf("Enter the total points: ");
-        scanf("%d", &points);
+        scanf("%d", &points);//read total points
 
-        print_combinations(points);
+        print_combinations(points);//do points calculations
     }
 
-    return 0;
+    return 0;//end main
 }
